59f8c89cf00a12111f8b50cfc631107dbce64c98
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-06-15 22:30:57 +0100

    Latest Commit

--- script.js
+++ script.js
@@ -14,22 +14,64 @@ const specialAttack = document.getElementById('special-attack')
 const specialDefense = document.getElementById('special-defense')
 const speed = document.getElementById('speed')
 
-const pokemons = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon'
+const pokemonsAPI  = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon'
 
+let pokemonsList = []; 
+let selectedPokemon = {}
+let selectedPokemonInfos = {}
 
-const fetchData = async () => {
+const fetchData = async (enteredPokemon) => {
   try {
-    const res = await fetch(pokemons)
+    const res = await fetch(pokemonsAPI)
     const data = await res.json()
-    getPokemonsInfos(data)
+    pokemonsList = getPokemons(data);
+    selectedPokemon = getSelectedPokemon(pokemonsList, enteredPokemon)
+    selectedPokemonInfos = await getSelectedPokemonInfos(selectedPokemon)
   } catch (error) {
     console.log(error)
   }
 }
 
-fetchData()
+searchButton.addEventListener("click", async () => {
+  const enteredPokemon = searchInput.value.toLowerCase()
+  await fetchData(enteredPokemon)
+  if (pokemonsList.some(pokemon => pokemon.name === enteredPokemon || pokemon.id == enteredPokemon)) {
+    displaySelectedPokemon(selectedPokemonInfos)
+  } else {
+    alert("Pokémon not found")
+  }
+})
 
-const getPokemonsInfos = (data) => {
+const getPokemons = (data) => {
   const { results } = data
-  
+  return results
+}
+
+const getSelectedPokemon = (pokemonsList, enteredPokemon) => {
+  let selectedPokemon = {}
+  pokemonsList.forEach(pokemon => {
+    if(pokemon.name === enteredPokemon || pokemon.id == enteredPokemon) {
+      selectedPokemon = {...pokemon}
+    }
+  })
+  return selectedPokemon
+}
+
+const getSelectedPokemonInfos = async (selectedPokemon) => {
+  const res = await fetch(selectedPokemon.url)
+  const data = await res.json()
+  return data
+}
+
+const displaySelectedPokemon = (selectedPokemonInfos) => {
+  const { id, name, weight: pokemonWeight, height: pokemonHeight, sprites, types: pokemonTypes, stats } = selectedPokemonInfos
+  pokemonId.textContent = `#${id}`
+  pokemonName.textContent = `${name.charAt(0).toUpperCase() + name.slice(1)}`
+  weight.textContent = `Weight: ${pokemonWeight}`
+  height.textContent = `Height: ${pokemonHeight}`
+  pokemonImg.innerHTML = `<img src="${sprites.front_default}" alt="${name}" id="sprite">`
+  types.innerHTML = pokemonTypes.map(type => `<div class="types ${type.type.name}">${(type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1))}</div>`).join('')
+  stats.forEach((stat) => {
+    document.getElementById(stat.stat.name).textContent = stat.base_stat;
+  })
 }
